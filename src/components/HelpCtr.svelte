bda09618a556c96790541f7df21d657baaebc248
Art Goldhammer <art.goldhammer@gmail.com>
2023-10-06 17:50:34 -0400

    clicking

--- src/components/HelpCtr.svelte
+++ src/components/HelpCtr.svelte
@@ -1,15 +1,32 @@
 <script lang="ts">
+  type MouseEvent = { currentTarget: HTMLDivElement };
+  function hdlClick(e: MouseEvent) {
+    console.log("clicked", e.currentTarget);
+    if (!e.currentTarget) {
+      throw new Error("null target");
+    } else {
+      const clickSrc = e.currentTarget;
+      console.log("clickSrc", clickSrc);
+      const parentElement = clickSrc.parentElement;
+      console.log("parent", parent);
+    }
+
+    // console.log("sibling", cardtop.nextSibling);
+  }
 </script>
 
 <div class="help-ctr">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+
   <div class="help-card">
-    <div class="help-card-top">
+    <div id="help-top-1" class="help-card-top">
       <p>HELP I need somebody</p>
-      <span class="open">&#10133; </span>
+      <div class="open" on:click|capture={hdlClick}>&#10133;</div>
     </div>
-    <div class="help-card-btm">
+    <div id="help-btm-1" class="help-card-btm">
       <p>not just anybody</p>
-      <span class="close">&#10060;</span>
+      <div class="close">&#10060;</div>
     </div>
   </div>
 </div>
@@ -30,6 +47,21 @@
   }
 
   .help-card-top {
+    display: grid;
+    grid-template-columns: 10fr 1fr;
     border-bottom: 2px solid blue;
+  }
+
+  .help-card-btm {
+    display: grid;
+    grid-template-columns: 10fr 1fr;
+  }
+
+  .open,
+  .close {
+    display: flex;
+    justify-content: center;
+    border: 2px solid black;
+    align-items: center;
   }
 </style>
