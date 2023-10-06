bdd08e8daafc91e30fb45567e401ef9899f6c4f7
Art Goldhammer <art.goldhammer@gmail.com>
2023-10-06 15:27:58 -0400

    added open/close icons

--- src/components/HelpCtr.svelte
+++ src/components/HelpCtr.svelte
@@ -1,5 +1,17 @@
+<script lang="ts">
+</script>
+
 <div class="help-ctr">
-  <p>HELP I need somebody</p>
+  <div class="help-card">
+    <div class="help-card-top">
+      <p>HELP I need somebody</p>
+      <span class="open">&#10133; </span>
+    </div>
+    <div class="help-card-btm">
+      <p>not just anybody</p>
+      <span class="close">&#10060;</span>
+    </div>
+  </div>
 </div>
 
 <style>
@@ -7,5 +19,17 @@
     height: 100vh;
     border: 2px solid red;
     border-radius: 8px;
+  }
+
+  .help-card {
+    padding: 8px;
+    margin: 8px;
+    width: 80%;
+    border: 2px solid blue;
+    border-radius: 8px;
+  }
+
+  .help-card-top {
+    border-bottom: 2px solid blue;
   }
 </style>
