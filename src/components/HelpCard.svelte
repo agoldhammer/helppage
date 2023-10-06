56c9978fbff742e6604dcb66e3a811104fd389f7
Art Goldhammer <art.goldhammer@gmail.com>
2023-10-06 19:45:58 -0400

    added slots, refactored

---
+++ src/components/HelpCard.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  //   type MouseEvent = { currentTarget: HTMLDivElement };
+  function hdlClick(e: MouseEvent) {
+    console.log("clicked", e.currentTarget);
+    if (!e.currentTarget) {
+      throw new Error("null target");
+    } else {
+      const clickSrc = e.currentTarget;
+      console.log("clickSrc", clickSrc);
+      const parent = clickSrc.parentElement;
+      console.log("parent", parent);
+    }
+
+    // console.log("sibling", cardtop.nextSibling);
+  }
+</script>
+
+<div class="help-card">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div id="help-top-1" class="help-card-top">
+    <!-- next do nothing line to suppress spurious warning -->
+    {#if false} <slot />{/if}
+
+    <slot name="topic" />
+    <button id="help-open-1" class="open" on:click|capture={hdlClick}>
+      &#10133;
+    </button>
+  </div>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div id="help-btm-1" class="help-card-btm">
+    <slot name="description" />
+    <button class="close" on:click|capture={hdlClick}>&#10060;</button>
+  </div>
+</div>
+
+<style>
+  .help-card {
+    padding: 8px;
+    margin: 8px;
+    width: 80%;
+    border: 2px solid blue;
+    border-radius: 8px;
+  }
+
+  .help-card-top {
+    display: grid;
+    grid-template-columns: 10fr 1fr;
+    border-bottom: 2px solid blue;
+    color: white;
+    background-color: blue;
+  }
+
+  .help-card-btm {
+    display: grid;
+    grid-template-columns: 10fr 1fr;
+    background-color: lightsalmon;
+  }
+
+  .open,
+  .close {
+    background-color: white;
+    color: red;
+    display: flex;
+    justify-content: center;
+    border: 2px solid black;
+    align-items: center;
+  }
+</style>
--- src/components/HelpCtr.svelte
+++ src/components/HelpCtr.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+  import HelpCard from "./HelpCard.svelte";
   type MouseEvent = { currentTarget: HTMLDivElement };
+  const topic1 = "Mollit magna commodo laborum aute.";
+  const topic2 =
+    "Deserunt adipisicing occaecat incididunt ullamco occaecat fugiat culpa nisi.";
+  const topic3 =
+    "Incididunt ullamco adipisicing excepteur laboris id consectetur dolor.";
+  const topics = [topic1, topic2, topic3];
   function hdlClick(e: MouseEvent) {
     console.log("clicked", e.currentTarget);
     if (!e.currentTarget) {
@@ -18,17 +25,22 @@
 <div class="help-ctr">
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <!-- svelte-ignore a11y-no-static-element-interactions -->
-
-  <div class="help-card">
-    <div id="help-top-1" class="help-card-top">
-      <p>HELP I need somebody</p>
-      <div class="open" on:click|capture={hdlClick}>&#10133;</div>
-    </div>
-    <div id="help-btm-1" class="help-card-btm">
-      <p>not just anybody</p>
-      <div class="close">&#10060;</div>
-    </div>
-  </div>
+  <HelpCard>
+    <p slot="topic">HELP I need somebody</p>
+    <p slot="description">Not just anybody</p>
+  </HelpCard>
+  {#each topics as topic}
+    <HelpCard>
+      <p slot="topic">{topic}</p>
+      <p slot="description">
+        Ut labore ex sint id excepteur. Cillum anim et ex consequat veniam nulla
+        est ea enim excepteur voluptate. Non officia esse velit sit non
+        cupidatat quis proident aute laborum sint. Qui excepteur tempor cillum
+        laboris est est.
+      </p>
+      />
+    </HelpCard>
+  {/each}
 </div>
 
 <style>
@@ -36,32 +48,6 @@
     height: 100vh;
     border: 2px solid red;
     border-radius: 8px;
-  }
-
-  .help-card {
-    padding: 8px;
-    margin: 8px;
-    width: 80%;
-    border: 2px solid blue;
-    border-radius: 8px;
-  }
-
-  .help-card-top {
-    display: grid;
-    grid-template-columns: 10fr 1fr;
-    border-bottom: 2px solid blue;
-  }
-
-  .help-card-btm {
-    display: grid;
-    grid-template-columns: 10fr 1fr;
-  }
-
-  .open,
-  .close {
-    display: flex;
-    justify-content: center;
-    border: 2px solid black;
-    align-items: center;
+    overflow-y: scroll;
   }
 </style>
